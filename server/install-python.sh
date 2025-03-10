ae4992c5e72e009ef4c41da9c131e25bd28f62eb
jlonic <josiplonic@hotmail.com>
2025-03-11 00:49:16 +0100

    new py ver test

--- server/install-python.sh
+++ server/install-python.sh
@@ -2,7 +2,7 @@
 
 # Update the package list and install Python 3 and pip
 apt-get update
-apt-get install -y python3 python3-pip
+apt-get install -y python3.8 python3.8-dev python3-pip
 
 # Install required Python libraries
 pip3 install billboard.py
