3eb3283a925211f40455147c48646e56126d1e6a
jlonic <josiplonic@hotmail.com>
2025-03-11 00:14:03 +0100

    test..

--- server/python-scripts/getTop100Artists.py
+++ server/python-scripts/getTop100Artists.py
@@ -1,18 +1,6 @@
 import billboard
 import json
 
-def patch_billboard_session(): #temporary fix for billboard.py library
-    original_get_session = billboard._get_session_with_retries
-
-    def new_get_session_with_retries(max_retries):
-        session = original_get_session(max_retries)
-        session.headers.update({'User-Agent': 'Mozilla/5.0'})
-        return session
-
-    billboard._get_session_with_retries = new_get_session_with_retries
-
-patch_billboard_session()
-
 #current weeks most popular 100 artists ranked by album and track sales across all genres
 def get_top_artists():
     chart = billboard.ChartData('artist-100')
