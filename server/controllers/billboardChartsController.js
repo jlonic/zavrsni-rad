398645ff32ad7a0b5213a4edd19bd11be75a447c
jlonic <josiplonic@hotmail.com>
2025-03-10 21:27:24 +0100

    charts fix?

--- server/controllers/billboardChartsController.js
+++ server/controllers/billboardChartsController.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 const getTopAlbums = async (_, res) => {
     try {
         // exec("/usr/src/app/venv/bin/python ./python-scripts/getTop200Albums.py", (error, data) => { //for running in docker
-        exec("python ./python-scripts/getTop200Albums.py", (error, data) => { //for running locally
+        exec("python3 ./python-scripts/getTop200Albums.py", (error, data) => { //for running locally
             if (error) {
                 return res.status(500).send({ message: "Error fetching top albums" });
             }
@@ -17,7 +17,7 @@ const getTopAlbums = async (_, res) => {
 const getHot100 = async (_, res) => {
     try {
         // exec("/usr/src/app/venv/bin/python ./python-scripts/getHot100.py", (error, data) => { //for running in docker
-        exec("python ./python-scripts/getHot100.py", (error, data) => { //for running locally
+        exec("python3 ./python-scripts/getHot100.py", (error, data) => { //for running locally
             if (error) {
                 return res.status(500).send({ message: "Error fetching top songs" });
             }
@@ -31,7 +31,7 @@ const getHot100 = async (_, res) => {
 const getGlobal200 = async (_, res) => {
     try {
         // exec("/usr/src/app/venv/bin/python ./python-scripts/getGlobal200.py", (error, data) => { //for running in docker
-        exec("python ./python-scripts/getGlobal200.py", (error, data) => { //for running locally
+        exec("python3 ./python-scripts/getGlobal200.py", (error, data) => { //for running locally
             if (error) {
                 return res.status(500).send({ message: "Error fetching top songs" });
             }
@@ -45,7 +45,7 @@ const getGlobal200 = async (_, res) => {
 const getTop100Artists = async (_, res) => {
     try {
         // exec("/usr/src/app/venv/bin/python ./python-scripts/getTop100Artists.py", (error, data) => { //for running in docker
-        exec("python ./python-scripts/getTop100Artists.py", (error, data) => { //for running locally
+        exec("python3 ./python-scripts/getTop100Artists.py", (error, data) => { //for running locally
             if (error) {
                 return res.status(500).send({ message: "Error fetching top artists" });
             }
