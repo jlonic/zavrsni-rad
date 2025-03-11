4ce41d0752e218ef894be2d63e3e696f642701d7
jlonic <josiplonic@hotmail.com>
2025-03-11 12:29:20 +0100

    test

--- server/controllers/billboardChartsController.js
+++ server/controllers/billboardChartsController.js
@@ -1,26 +1,31 @@
 const { exec } = require("child_process");
-const pythonPath = '/usr/bin/python';
-const pythonPath2 = '/usr/bin/python3';
-const pythonPath3 = "/usr/src/app/venv/bin/python";
-const pythonPath4 = "/usr/src/app/venv/bin/python3";
+
 const getTopAlbums = async (_, res) => {
     try {
-        // exec("/usr/src/app/venv/bin/python ./python-scripts/getTop200Albums.py", (error, data) => { //for running in docker
-        exec(`${pythonPath} ./python-scripts/getTop200Albums.py`, (error, data) => { //for running locally
+        console.log("Running Python script...");
+        exec("/usr/src/app/venv/bin/python ./python-scripts/getTop200Albums.py", (error, data, stderr) => { //for running in docker
+        // exec(`python ./python-scripts/getTop200Albums.py`, (error, data) => { //for running locally
+            console.log("Python script finished running...");
+
             if (error) {
+                console.error(`Exec error: ${error.message}`);
+                console.error(`Stderr: ${stderr}`);
                 return res.status(500).send({ message: "Error fetching top albums" });
             }
+            console.log("Python Output:", data);
+            console.log("Stderr:", stderr);
             res.json(JSON.parse(data));
         });
     } catch (error) {
+        console.error(`Catch error: ${error.message}`);
         res.status(500).json({ message: "Error retrieving charts" });
     }
 };
 
 const getHot100 = async (_, res) => {
     try {
-        // exec("/usr/src/app/venv/bin/python ./python-scripts/getHot100.py", (error, data) => { //for running in docker
-        exec(`${pythonPath2} ./python-scripts/getHot100.py`, (error, data) => { //for running locally
+        exec("/usr/src/app/venv/bin/python ./python-scripts/getHot100.py", (error, data) => { //for running in docker
+        // exec(`python ./python-scripts/getHot100.py`, (error, data) => { //for running locally
             if (error) {
                 return res.status(500).send({ message: "Error fetching top songs" });
             }
@@ -33,8 +38,8 @@ const getHot100 = async (_, res) => {
 
 const getGlobal200 = async (_, res) => {
     try {
-        // exec("/usr/src/app/venv/bin/python ./python-scripts/getGlobal200.py", (error, data) => { //for running in docker
-        exec(`${pythonPath3} ./python-scripts/getGlobal200.py`, (error, data) => { //for running locally
+        exec("/usr/src/app/venv/bin/python ./python-scripts/getGlobal200.py", (error, data) => { //for running in docker
+        // exec(`python ./python-scripts/getGlobal200.py`, (error, data) => { //for running locally
             if (error) {
                 return res.status(500).send({ message: "Error fetching top songs" });
             }
@@ -47,8 +52,8 @@ const getGlobal200 = async (_, res) => {
 
 const getTop100Artists = async (_, res) => {
     try {
-        // exec("/usr/src/app/venv/bin/python ./python-scripts/getTop100Artists.py", (error, data) => { //for running in docker
-        exec(`${pythonPath4} ./python-scripts/getTop100Artists.py`, (error, data) => { //for running locally
+        exec("/usr/src/app/venv/bin/python ./python-scripts/getTop100Artists.py", (error, data) => { //for running in docker
+        // exec(`python ./python-scripts/getTop100Artists.py`, (error, data) => { //for running locally
             if (error) {
                 console.error(`Exec error: ${error.message}`);
                 return res.status(500).send({ message: "Error fetching top artists" });
